c9c80f8de752ca67f7f4364c0e548bc274a34c73
Nabemh <nhanior@gmail.com>
2025-06-19 11:08:07 +0100

    feedback

--- frontend/app/components/sidebar.tsx
+++ frontend/app/components/sidebar.tsx
@@ -9,12 +9,11 @@ export default function Sidebar() {
                 <ul className='flex-column gap2'>
                     <li className='logoArea1' ><Logo /></li>
                     <li title='Overview'><Link href='/overview'><Image src='/images/icons/dashboard.svg' alt='' width={25} height={25} /> Overview</Link></li>
+                    <li title='Progress'><Link href='/progress'><Image src='/images/icons/progress.svg' alt='' width={25} height={25} /> CGPA Tracker</Link></li>
                     <li title='Workout'><Link href='/workout'><Image src='/images/icons/workout.svg' alt='' width={25} height={25} /> Workout</Link></li>
-                    <li title='Diet Plans'><Link href='/diet-plan'><Image src='/images/icons/dietplan.svg' alt='' width={25} height={25} /> Diet Plan</Link></li>
-                    <li title='Recipes'><Link href='/recipes'><Image src='/images/icons/dietplan.svg' alt='' width={25} height={25} />Recipes</Link></li>
+                    <li title='Recipes'><Link href='/recipes'><Image src='/images/icons/dietplan.svg' alt='' width={25} height={25} />To-Do</Link></li>
                     <hr />
                     <li title='Goals'><Link href='/goals'><Image src='/images/icons/goal.svg' alt='' width={25} height={25} /> Goals</Link></li>
-                    <li title='Progress'><Link href='/progress'><Image src='/images/icons/progress.svg' alt='' width={25} height={25} /> Progress</Link></li>
                     <li title='Contact Us'><Link href='/contact'><Image src='/images/icons/contact.svg' alt='' width={25} height={25} /> Contact Us</Link></li>
                     <li title='Help'><Link href='/help'><Image src='/images/icons/help.svg' alt='' width={25} height={25} /> Help</Link></li>
                     <li title='My Profile'><Link href='/my-profile'><Image src='/images/icons/user.svg' alt='' width={25} height={25} /> My Account</Link></li>
--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,60 +1,42 @@
 import Image from "next/image";
-import style from './contact.module.css';
+import styles from './contact.module.css';
 
 export default function Contact() {
   return (
-    <>
-      <div className={`flex-center flex-column ${style.contact}`}>
-        <div className={`pt3 pb3 ${style.heading}`}>
-          <h1>Contact Our Friendly Team</h1>
-        </div>
-        <p className="pt1">Let us know how can we help</p>
-        <div className={`flex-space-around pt3 pb3 gap5 ${style.info}`}>
-          <div className={`flex-column gap2 ${style.getInTouch}`}>
-            <div className={`flex gap1 ${style.contactInfo}`}>
-              <Image src='/images/icons/location.svg' alt="" width={30} height={30} />
-              <div className={`flex-coloumn ${style.data}`}>
-                <h2>Visit Us</h2>
-                <p>visit our office</p>
-                <span>Directions Google Map</span>
-              </div>
-            </div>
-            <div className={`flex gap1 ${style.contactInfo}`}>
-              <Image src='/images/icons/phone.svg' alt="" width={30} height={30} />
-              <div className={`flex-coloumn ${style.data}`}>
-                <h2>Call Us</h2>
-                <p>Mon-Sat (8:00 a.m. - 11:00 p.m.)</p>
-                <span>+91 82396 10162</span>
-              </div>
-            </div>
-            <div className={`flex gap1 ${style.contactInfo}`}>
-              <Image src='/images/icons/mail.svg' alt="" width={30} height={30} />
-              <div className={`flex-coloumn ${style.data}`}>
-                <h2>Email</h2>
-                <p>We are here to help</p>
-                <span>contactpiotechindia@gmail.com</span>
-              </div>
-            </div>
-            <div className={`flex gap1 ${style.contactInfo}`}>
-              <Image src='/images/icons/mail.svg' alt="" width={30} height={30} />
-              <div className={`flex-coloumn ${style.data}`}>
-                <h2>Chat to support</h2>
-                <p>Speak to our friendly team</p>
-                <span>+91 82396 10162</span>
-              </div>
+    <div className={`flex-center flex-column ${styles.contact}`}>
+      <div className={`pt3 pb3 ${styles.heading}`}>
+        <h1>We Value Your Feedback</h1>
+      </div>
+      
+      <div className={`flex-center flex-column ${styles.contactFormContainer}`}>
+        <form className={`flex-column gap2 ${styles.feedback}`} action="" method="post">
+          {/* Email contact moved here */}
+          <div className={`flex-center gap1 ${styles.contactInfo}`}>
+            <Image src='/images/icons/mail.svg' alt="Email icon" width={40} height={40} />
+            <div className={`flex-column ${styles.data}`}>
+              <h2>Email Us</h2>
+              <p>We are here to help</p>
+              <span>contactacademicsuccess@gmail.com</span>
             </div>
           </div>
-          <form className={`flex-column gap1 ${style.feedback}`} action="" method="post" >
-            <label>Your Name: </label>
-            <input className="pt1 pb1 pr2 pl2" type="text" name="yourName" value="" />
-            <label>Your Email: </label>
-            <input className="pt1 pb1 pr2 pl2" type="email" name="yourEmail" value="" />
-            <label>Your Message: </label>
-            <textarea className="pt1 pb1 pr2 pl2" name="Name"></textarea>
-            <button className={`pt1 pb1 pr3 pl3 ${style.submit}`} type="submit" name="">Send Message</button>
-          </form>
-        </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel}>Your Message:</label>
+            <textarea 
+              className={`${styles.formTextarea} ${styles.largeTextarea}`} 
+              name="message" 
+              required
+            ></textarea>
+          </div>
+
+          <button 
+            className={`${styles.submitButton} ${styles.largeButton}`} 
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
